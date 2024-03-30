692b0f8783b4a5aa90478ee82994296364dd0313
sgokul961 <sgokul961@gmail.com>
2024-03-30 18:03:24 +0530

    small changs made

--- pkg/helper/likeconsumer.go
+++ pkg/helper/likeconsumer.go
@@ -1,70 +1,63 @@
 package helper
 
 import (
-	"encoding/json"
-	"fmt"
-	"os"
-	"os/signal"
-	"syscall"
-
 	"github.com/IBM/sarama"
-	"github.com/sgokul961/echo-hub-notification-svc/pkg/models"
 )
 
-func StartLikeKafkaConsumer() {
-	brokerUrl := []string{"localhost:9092"}
-	// Use the same Kafka broker URL as the producer
+// func StartLikeKafkaConsumer() {
+// 	brokerUrl := []string{"localhost:9092"}
+// 	// Use the same Kafka broker URL as the producer
 
-	topic := "like_notification"
+// 	topic := "like_notification"
 
-	worker, err := ConnectToLikeConsumer(brokerUrl)
-	if err != nil {
-		panic(err)
-	}
+// 	worker, err := ConnectToLikeConsumer(brokerUrl)
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
-	partitionConsumer, err := worker.ConsumePartition(topic, 0, sarama.OffsetOldest)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("consumer started")
+// 	partitionConsumer, err := worker.ConsumePartition(topic, 0, sarama.OffsetOldest)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	fmt.Println("consumer started")
 
-	signchan := make(chan os.Signal, 1)
-	signal.Notify(signchan, syscall.SIGINT, syscall.SIGTERM)
-	messagecount := 0
+// 	signchan := make(chan os.Signal, 1)
+// 	signal.Notify(signchan, syscall.SIGINT, syscall.SIGTERM)
+// 	messagecount := 0
 
-	go func() {
-		for {
-			select {
-			case err := <-partitionConsumer.Errors():
-				fmt.Println("Error:", err)
-			case msg := <-partitionConsumer.Messages():
+// 	go func() {
+// 		for {
+// 			select {
+// 			case err := <-partitionConsumer.Errors():
+// 				fmt.Println("Error:", err)
+// 			case msg := <-partitionConsumer.Messages():
 
-				var message models.LikeNotification
+// 				var message models.LikeNotification
 
-				if err := json.Unmarshal(msg.Value, &message); err != nil {
-					fmt.Println("Error parsing message:", err)
-					continue
-				}
+// 				if err := json.Unmarshal(msg.Value, &message); err != nil {
+// 					fmt.Println("Error parsing message:", err)
+// 					continue
+// 				}
 
-				messagecount++
-				fmt.Printf("received message count: %d | Topic: %s | Message: %s\n", messagecount, string(msg.Topic), string(msg.Value))
+// 				messagecount++
+// 				fmt.Printf("received message count: %d | Topic: %s | Message: %s\n", messagecount, string(msg.Topic), string(msg.Value))
 
-			case <-signchan:
-				fmt.Println("interruption detected")
-				donech <- struct{}{}
-			}
-		}
-	}()
+// 			case <-signchan:
+// 				fmt.Println("interruption detected")
+// 				donech <- struct{}{}
+// 			}
+// 		}
+// 	}()
 
-	<-donech
+// 	<-donech
 
-	fmt.Println("processed", messagecount, "messages")
-	if err := worker.Close(); err != nil {
-		panic(err)
-	}
-}
+// 	fmt.Println("processed", messagecount, "messages")
+// 	if err := worker.Close(); err != nil {
+// 		panic(err)
+// 	}
+// }
 
-var donech = make(chan struct{})
+//var donech = make(chan struct{})
 
 func ConnectToLikeConsumer(brokerUrl []string) (sarama.Consumer, error) {
 	config := sarama.NewConfig()
